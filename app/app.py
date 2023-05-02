513059230f6f64dd68b0fa6be004f56bf0dbc5a0
patod01 <patocv2@hotmail.com>
2023-05-02 18:10:43 -0400

    mi0.3.0 - Connection to database
    - `app.py` now can infer the db by its own.
    - `.gitignore` updated for pycache and journal files.
    
    This minor update brings full coverage of db connection, which means
    that the app first looks for a production database and, if fails,
    try to create a new one for testing purposes.
    
    This is the final update prior to start working at the docker update
    and the change of the metal infrastructure.

--- app/app.py
+++ app/app.py
@@ -7,7 +7,42 @@ from flask_socketio import SocketIO, emit
 
 
 ### Basic setup ###
-con = sqlite3.connect('remo.db')
+data_base = 'remo.db'
+
+if not os.path.isfile(data_base):
+     data_base = 'test.db'
+
+if not os.path.isfile(data_base):
+     ### Setup ###
+     with open('query/schema.sql') as schema:
+          schema = ''.join(schema.readlines())
+          # print(schema)
+     with open('query/list_view.sql') as v_list:
+          v_list = ''.join(v_list.readlines())
+          # print(v_list)
+     with open('query/remotes_view.sql') as v_remotes:
+          v_remotes = ''.join(v_remotes.readlines())
+          # print(v_remotes)
+     with open('query/count_view.sql') as v_count:
+          v_count = ''.join(v_count.readlines())
+          # print(v_count)
+
+     setup_query = 'BEGIN TRANSACTION;' + '\n' + schema + '\n' + v_list + '\n' + v_remotes + '\n' + v_count + 'COMMIT;'
+     print(setup_query)
+
+     with sqlite3.connect(data_base) as con:
+          con.executescript(setup_query)
+
+     ### Mock ###
+     with open('query/moco.sql') as mock:
+          mock = ''.join(mock.readlines())
+          print(mock)
+
+     with sqlite3.connect(data_base) as con:
+          con.executescript(mock)
+
+con = sqlite3.connect(data_base)
+
 
 app = Flask(
      __name__,
