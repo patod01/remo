08910675534ede84b8a2b776ad886a25fc2e5094
patod01 <patocv2@hotmail.com>
2023-05-20 01:01:11 -0400

    pa0.3.1 - Improved run files for windows and linux
    - `run.bat` now builds the environment for windows to run the app.
    - `run.sh` created for linux to run the app over docker.

--- app/run.bat
+++ app/run.bat
@@ -1,5 +1,20 @@
 @echo off
 
+:: configuracion de entorno para windows
+if not exist ..\.env\Scripts\python.exe (
+     echo Installanding Piton...
+     python -m venv ..\.env
+     ..\.env\Scripts\activate.bat
+     python -m pip install --upgrade pip
+     pip install --no-cache-dir -r req.txt
+     echo.
+     echo escrit de instalacion terminado!!
+     pause>nul
+     goto ned
+)
+
 :: por defecto, debe encontrarse en modo 'dev'
 ..\.env\Scripts\python.exe app.py dev
 rem ..\.env\Scripts\python.exe app.py Gevent
+
+:ned
---
+++ app/run.sh
@@ -0,0 +1,21 @@
+#! /bin/ash
+
+docker network create alambre
+
+docker run --rm -d \
+--network alambre \
+--name flan \
+cloudflare/cloudflared:latest \
+tunnel --no-autoupdate run --token $TOKEN
+
+docker run --rm -d \
+--network alambre \
+--name mac \
+-p 10011:10011 \
+-w /app \
+-v "$(pwd)":/app \
+python:3.11.2-alpine3.17 \
+ash -c " \
+pip install --upgrade pip && \
+pip install --no-cache-dir -r req.txt \
+"
