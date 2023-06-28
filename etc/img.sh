3b13429a724efcac462d0f28d07fdc662da90a9c
patod01 <patocv2@hotmail.com>
2023-06-28 14:47:44 -0400

    pa0.5.16 - Updates `img.sh` to work with a newer version of buildx

--- etc/img.sh
+++ etc/img.sh
@@ -4,11 +4,13 @@ modo=$1
 VER=$2
 push=$3
 
+BUILDX_V="0.11.0"
+
 if [ "$modo" = "install" ]; then
      echo Installando buildx en Docker!
      # install buildx
      echo mkdir -p ~/.docker/cli-plugins
-     echo curl -SL https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-amd64 -o ~/.docker/cli-plugins/docker-buildx
+     echo curl -SL https://github.com/docker/buildx/releases/download/v$BUILDX_V/buildx-v$BUILDX_V.linux-amd64 -o ~/.docker/cli-plugins/docker-buildx
      echo chmod a+x ~/.docker/cli-plugins/docker-buildx
      echo docker buildx create --driver=docker-container --name=slave
      echo Installacion terminada
